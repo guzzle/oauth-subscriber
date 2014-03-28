94c8dd681f5f275eb571690a12a2e8bf6529e04e
David de Boer <david@ddeboer.nl>
2014-03-28 21:14:21 +0100

    Start OAuth2 implementation
    Fix CS

---
+++ src/GrantType/GrantTypeInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GuzzleHttp\Subscriber\Oauth\GrantType;
+
+interface GrantTypeInterface
+{
+    public function getToken();
+}
---
+++ src/Oauth2.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace GuzzleHttp\Subscriber\Oauth;
+
+use GuzzleHttp\Event\BeforeEvent;
+use GuzzleHttp\Event\RequestEvents;
+use GuzzleHttp\Event\SubscriberInterface;
+use GuzzleHttp\Subscriber\Oauth\GrantType\GrantTypeInterface;
+
+class Oauth2 implements SubscriberInterface
+{
+    protected $accessToken;
+    protected $grantType;
+
+    public function __construct(GrantTypeInterface $grantType = null)
+    {
+        $this->grantType = $grantType;
+    }
+
+    public function getEvents()
+    {
+        return ['before' => ['onBefore', RequestEvents::SIGN_REQUEST]];
+    }
+
+    public function onBefore(BeforeEvent $event)
+    {
+        $token = $this->getAccessToken();
+        $header = $this->getAuthorizationHeader($token);
+
+        $event->getRequest()->setHeader('Authorization', $header);
+    }
+
+    /**
+     * Set access token
+     *
+     * @param string $accessToken
+     */
+    public function setAccessToken($accessToken)
+    {
+        $this->accessToken = $accessToken;
+    }
+
+    /**
+     * Get access token
+     *
+     * @return string
+     */
+    public function getAccessToken()
+    {
+        if (null === $this->accessToken) {
+            if ($this->grantType) {
+                $this->accessToken = $this->grantType->getToken();
+            }
+        }
+
+        return $this->accessToken;
+    }
+
+    protected function getAuthorizationHeader(AccessToken $token)
+    {
+        return sprintf('Bearer %s', $token->getToken());
+    }
+}
