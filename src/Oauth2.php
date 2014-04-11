ad487c7171fa502849fd1246ecb169fe626b1374
David de Boer <david@ddeboer.nl>
2014-04-11 14:31:49 +0200

    Retry on authentication failure

--- src/Oauth2.php
+++ src/Oauth2.php
@@ -3,8 +3,10 @@
 namespace GuzzleHttp\Subscriber\Oauth;
 
 use GuzzleHttp\Event\BeforeEvent;
+use GuzzleHttp\Event\ErrorEvent;
 use GuzzleHttp\Event\RequestEvents;
 use GuzzleHttp\Event\SubscriberInterface;
+use GuzzleHttp\Message\RequestInterface;
 use GuzzleHttp\Subscriber\Oauth\GrantType\GrantTypeInterface;
 
 class Oauth2 implements SubscriberInterface
@@ -19,14 +21,17 @@ class Oauth2 implements SubscriberInterface
 
     public function getEvents()
     {
-        return ['before' => ['onBefore', RequestEvents::SIGN_REQUEST]];
+        return [
+            'before' => ['onBefore', RequestEvents::SIGN_REQUEST],
+            'error'  => ['onError', RequestEvents::EARLY],
+        ];
     }
 
     public function onBefore(BeforeEvent $event)
     {
         $request = $event->getRequest();
 
-        // Only sign requests using "auth"="oauth"
+        // Only sign requests using "auth"="oauth2"
         if ($request->getConfig()->get('auth') != 'oauth2') {
             return;
         }
@@ -35,6 +40,20 @@ class Oauth2 implements SubscriberInterface
         $header = $this->getAuthorizationHeader($token);
 
         $request->setHeader('Authorization', $header);
+    }
+
+    public function onError(ErrorEvent $event)
+    {
+        if (401 == $event->getResponse()->getStatusCode()) {
+            $request = $event->getRequest();
+            if (!$request->getConfig()->get('retried')) {
+                if ($this->acquireAccessToken()) {
+                    $request->getConfig()->set('retried', true);
+                    $this->setHeader($request);
+                    $event->intercept($event->getClient()->send($request));
+                }
+            }
+        }
     }
 
     /**
@@ -55,16 +74,30 @@ class Oauth2 implements SubscriberInterface
     public function getAccessToken()
     {
         if (null === $this->accessToken) {
-            if ($this->grantType) {
-                $this->accessToken = $this->grantType->getToken();
-            }
+            $this->acquireAccessToken();
         }
 
         return $this->accessToken;
     }
 
+    private function setHeader(RequestInterface $request)
+    {
+        $token = $this->getAccessToken();
+        $header = $this->getAuthorizationHeader($token);
+        $request->setHeader('Authorization', $header);
+    }
+
     private function getAuthorizationHeader(AccessToken $token)
     {
         return sprintf('Bearer %s', $token->getToken());
+    }
+
+    private function acquireAccessToken()
+    {
+        if ($this->grantType) {
+            $this->accessToken = $this->grantType->getToken();
+        }
+
+        return $this->accessToken;
     }
 }
